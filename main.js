7b6fcadd1524b888d48fea28b3c3720dbe06bbd6
XxXEnderDragonSlayer07XxX/draken <tor.wallberg@elev.ga.ntig.se>
2026-02-19 10:26:32 +0100

    la in bild och bild folder

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
 const canvas = document.getElementById('menueCanvas');
 const ctx = canvas.getContext('2d');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-// cursor size
-const radius = 5;
 
+
+
+const targets = [
+  { size: 20, points: 10, src: 'images/larsonPrison.png' },
+];
+
+
+
+
+
+const radius = 5; //cursor storlek
+const lerpSpeed = 0.8; // hur tight den är med musen
 
 let mouseX = canvas.width / 2;
 let mouseY = canvas.height / 2;
 
+let currentX = mouseX;
+let currentY = mouseY;
+// gör så att musen föjer "musen" vart du än är
 window.addEventListener('mousemove', (e) => {
   mouseX = e.clientX;
   mouseY = e.clientY;
 });
 
-let currentX = mouseX;
-let currentY = mouseY;
-
+// om du klickar så är musen där du klickar
+window.addEventListener('mousedown', () => {
+  currentX = mouseX;
+  currentY = mouseY;
+});
+// målar
 function drawCursor(x, y) {
   ctx.beginPath();
   ctx.arc(x, y, radius, 0, Math.PI * 2);
@@ -29,8 +45,8 @@ function drawCursor(x, y) {
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  currentX += (mouseX - currentX) * 0.8;
-  currentY += (mouseY - currentY) * 0.8;
+  currentX += (mouseX - currentX) * lerpSpeed;
+  currentY += (mouseY - currentY) * lerpSpeed;
 
   drawCursor(currentX, currentY);
   requestAnimationFrame(animate);
