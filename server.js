0e9f9e111606539e8aa6f1c65b35e72fd304fd12
XxXEnderDragonSlayer07XxX/draken <tor.wallberg@elev.ga.ntig.se>
2026-05-28 10:42:53 +0200

    fix deploy

--- server.js
+++ server.js
@@ -35,6 +35,8 @@ app.get("/timer", (req, res) => {
 
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Running on ${PORT}`);
 });
