8ec5560b52e79185faf4a43e704014c979aeeaa4
XxXEnderDragonSlayer07XxX/draken <tor.wallberg@elev.ga.ntig.se>
2026-05-28 14:32:29 +0200

    fixa port issue

--- server.js
+++ server.js
@@ -2,30 +2,43 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
 app.use(cors());
 
-let gameDuration = 30;
-let timeLeft = gameDuration;
+let timeLeft = 30;
 let timerRunning = false;
+let interval;
 
 // Start timer
+app.get("/start-timer", (req, res) => {
+  const mode = req.query.mode;
+
+  let duration = 30;
+
+  if (mode === "easy") duration = 40;
+  if (mode === "medium") duration = 30;
+  if (mode === "hard") duration = 20;
+
+  timeLeft = duration;
+  timerRunning = true;
+
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    if (timeLeft > 0) {
+      timeLeft--;
+    } else {
+      clearInterval(interval);
+      timerRunning = false;
+    }
+  }, 1000);
+
+  res.json({ success: true, timeLeft });
+});
+
+// Get timer
 app.get("/timer", (req, res) => {
-
-    const mode = req.query.mode;
-  
-    let duration = 30;
-  
-    if (mode === "easy") duration = 40;
-    if (mode === "medium") duration = 30;
-    if (mode === "hard") duration = 20;
-  
-    res.json({
-      timeLeft,
-      mode
-    });
-  });
-
+  res.json({ timeLeft });
+});
 
 const PORT = process.env.PORT || 3000;
 
